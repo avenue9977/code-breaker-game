7bc9da0d8b1d60a99294e4c02094d990ca60d5e3
Nikola Chetelyazov <avenue9977@gmail.com>
2018-04-04 18:39:03 +0300

    Complete rewrite of the main.js

--- main.js
+++ main.js
@@ -4,66 +4,127 @@ let gameNumber,
     guess = document.getElementById('your-guess'),
     submit = document.getElementById('submit-guess'),
     results = document.getElementById('results'),
-    code = document.getElementById('code'),
-    alert = $('#warning'),
-    turns = 0,
-    correct = 0;
+    hiddenCode = document.getElementById('code'),
+    playAgainButton = document.getElementById('again'),
+    inputForm = document.getElementById('input-form'),
+    answers = document.getElementsByClassName('res'),
+    success = document.getElementById('success'),
+    warning = document.getElementById('warning'),
+    gameOverWarning = document.getElementById('game-over'),
+    turns = 0;
 
-// generate random 4 digits number
+// initialize the game number
+gameNumber = generateFourRandomNumbers();
 
-function randomNumberGenerator() {
-  let number = Math.floor(Math.random() * 10000).toString();
-  if (number.length < 4) {
-    number = '0' + number;
-  }
-  return number;
+function startTheGame() {
+
+    // get input data
+    let userInput = guess.value;
+
+    // validate users input
+    if (userInput === '' || userInput.length > 4 || userInput.length < 4) {
+        // show warning
+        warning.classList.add('show');
+
+    } else {
+        // hide warning if shown
+        warning.classList.remove('show');
+
+        html = displayResults(userInput, gameNumber);
+
+        // display result
+        results.innerHTML += html;
+
+        //count the turns
+        turns++;
+
+        if (turns >= 10) {
+            gameOver();
+        }
+
+        // check if results win
+        if (isWining()) {
+            winTheGame();
+        }
+
+    }
 }
 
-// store the game number
+// generate random 4 digits 
+// returns String
+function generateFourRandomNumbers() {
+    let number = Math.floor(Math.random() * 10000).toString();
+    if (number.length < 4) {
+        number = '0' + number;
+    }
+    return number;
+}
 
-gameNumber = randomNumberGenerator();
-
-function answerSubmit() {
-
-  // get input data
-  let userInput = guess.value;
-
-  // validate users input
-  if (userInput == '' || userInput.length > 4 || userInput.length < 4) {
-    // show warning
-    alert.addClass('show');
-
-  } else {
-
-    // hide warning if shown
-    alert.removeClass('show');
-
-    // compare input data
-    let html = '<div class="row"><div class="col-sm-6">' + userInput + '</div><div class="col-sm-6">';
+// display the results to the user
+// returns String
+function displayResults(userInput, gameNumber) {
+    let html = '<div class="row"><div class="col-sm-6">' + userInput + '</div><div class="res col-sm-6">';
 
     for (var i = 0; i < 4; i++) {
-      if (userInput.charAt(i) == gameNumber.charAt(i)) {
-        html += '<i class="fas fa-check" style="color: #9EBF5C"></i>';
-        correct++;
-      } else if (gameNumber.indexOf(userInput.charAt(i)) > -1 ) {
-        html += '<i class="fas fa-retweet" style="color: #EFAC19"></i>';
-      } else {
-        html += '<i class="fas fa-times" style="color: #E84545"></i>';
-      }
-
-      // if the 4 digits match player wins
-      if (correct == 4) {
-        code.style.color = "#9EBF5C";
-        code.textContent = userInput;
-      }
+        if (userInput.charAt(i) == gameNumber.charAt(i)) {
+            html += '<i class="fas fa-check correct"></i>';
+        } else if (gameNumber.indexOf(userInput.charAt(i)) > -1) {
+            html += '<i class="fas fa-retweet"></i>';
+        } else {
+            html += '<i class="fas fa-times"></i>';
+        }
     }
+
     html += '</div></div><hr>';
-
-  // display result
-    turns++;
-    console.log(turns);
-    results.innerHTML += html;
-  }
-
-
+    return html;
 }
+
+// is the submited number equal to the game number
+// returns boolean
+function isWining() {
+    if (answers.length > 0) {
+        var correct = 0;
+        for (var i = 0; i < 4; i++) {
+            // always check the last item in the answers array
+            var correctAnswer = answers[answers.length - 1].children[i].className.includes("correct");
+
+            if (correctAnswer) {
+                correct++;
+            }
+
+            if (correct === 4) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+function gameOver() {
+    gameOverWarning.classList.remove('hide');
+    gameOverWarning.classList.add('show');
+    playAgainButton.classList.remove('hide');
+    playAgainButton.classList.add('show');
+    inputForm.classList.add('hide');
+    code.style.animationName = "none";
+    hiddenCode.style.color = "#dc3545";
+    hiddenCode.textContent = gameNumber;
+    submit.disabled = true;
+}
+
+function winTheGame() {
+    inputForm.classList.add('hide');
+    success.classList.add('show');
+    success.classList.remove('hide');
+    playAgainButton.classList.remove('hide');
+    playAgainButton.classList.add('show');
+    code.style.animationName = "none";
+    hiddenCode.style.color = "#9EBF5C";
+    hiddenCode.textContent = gameNumber;
+}
+
+playAgainButton.addEventListener('click', function() {
+    window.location.reload();
+});
+
+submit.addEventListener('click', startTheGame);
